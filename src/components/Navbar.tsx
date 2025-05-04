8c60c34748208996d26225fa516f476b5ca1a7f2
Zicheng Liang <liangzicheng02@gmail.com>
2025-05-04 11:32:31 +0100

    Fixed build errors, ready for deployment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <nav className="w-full p-4 bg-white border-b shadow-sm flex justify-between">
-            <div className="font-bold text-black text-xl">Zicheng's Space</div>
+            <div className="font-bold text-black text-xl">Zicheng&#39;s Space</div>
             <div className="space-x-4">
                 <Link href="/" className="text-gray-700 hover:text-blue-500">
                 Homepage
