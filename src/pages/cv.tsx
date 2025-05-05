6e64ccab37c54a21c18edf3eb0ace4383a568a98
Zicheng Liang <liangzicheng02@gmail.com>
2025-05-05 22:19:27 +0100

    Added the CV page

---
+++ src/pages/cv.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function CV() {
+    return(
+        <object data="/CV_Latest.pdf" type="application/pdf" width="100%" height="1125">
+            <p>Alternative text - include a link <a href="https://drive.google.com/file/d/1QsaCQixjy3NoohuEd5iBsWJ9SaxirrJn/view?usp=sharing">to the PDF!</a></p>
+        </object>
+    );
+}
