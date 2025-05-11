2454ae13dc93bc4c65d73e637dc7df7b63e4f4b8
Zicheng Liang <liangzicheng02@gmail.com>
2025-05-11 09:54:59 +0100

    Fixed build errors

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { bio } from "../data/bios";
 
 export default function Main() {
   return (
     <main id="content" className="pt-24 lg:w-[52%] lg:py-24">
       <section id="about">
         <p>
-          Welcome to my website, here I’m not trying to impress with flashy stacks or big claims. Instead, I want this space to reflect how I think, how I work, and how I learn.
+          Welcome to my website, here I&#39;m not trying to impress with flashy stacks or big claims. Instead, I want this space to reflect how I think, how I work, and how I learn.
         </p>
         <br />
         <p>
@@ -14,11 +13,11 @@ export default function Main() {
         </p>
         <br />
         <p>
-          For example, you'll find how I approached building an agentic music generation workflow — what worked, what didn’t, and how I refined it over time. Or how I designed MiaoNance, a smart notebook that weaves together personal research and live crypto market data, inspired by the needs of investors like my mother.
+          For example, you&#39;ll find how I approached building an agentic music generation workflow — what worked, what didn&#39;t, and how I refined it over time. Or how I designed MiaoNance, a smart notebook that weaves together personal research and live crypto market data, inspired by the needs of investors like my mother.
         </p>
         <br/>
         <p>
-         My hope is that, as you explore this site, you’ll get a sense of how I work—with care, adaptability, and a steady sense of ownership over the things I build. 
+         My hope is that, as you explore this site, you will get a sense of how I work—with care, adaptability, and a steady sense of ownership over the things I build. 
         </p>
       </section>
       <br/>
