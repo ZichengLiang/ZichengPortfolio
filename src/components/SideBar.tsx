ac7165c9badb9826183a8878208dae818d7f9d1c
Zicheng Liang <liangzicheng02@gmail.com>
2025-05-11 09:48:58 +0100

    Make a better personal description Fixes #1

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import RoadSign from "./RoadSign";
 import Link from "next/link";
-import { shortBio } from "@/data/bios";
 
-// 为啥我的铺不满下边的屏幕呢？是不是因为内容不够？
 export default function SideBar() {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
@@ -14,7 +12,7 @@ export default function SideBar() {
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
           TCD Computer Science Undergraduate
         </h2>
-        <p className="mt-4 max-w-xs leading-normal">{shortBio}</p>
+        <p className="mt-4 max-w-xs leading-normal">I like building things that feel calm, even when the problem wasn’t.</p>
         <nav className="nav hidden lg:block" aria-label="In-page jump links">
           <ul className="mt-16 w-max">
             {RoadSign("About")}
--- src/data/bios.ts
+++
@@ -1,2 +0,0 @@
-export const shortBio:string = "The website features my project details.";
-export const bio:string = "还没想好呢 ";
